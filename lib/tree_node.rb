fd62b7ed4389db5b10f241bdfa45f65c95830af4
Ahmed <ahmed96razi39@gmail.com>
2023-02-17 14:16:25 -0800

    second commit

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -1,3 +1,23 @@
 class PolyTreeNode
   
+  attr_reader :value, :parent, :children
+
+  def initialize(value, parent = nil, children = [])
+    @value = value
+    @parent = parent
+    @children = children
+  end
+
+  def parent=(par)
+
+    @parent = par
+    
+  end
+
+  def add_child(child)
+    @children << child
+    self   
+  end
+
+
 end
