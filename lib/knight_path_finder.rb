8626ca9b39ae4a6c3c37a2095fa54334290683c1
bird <mbird3@gmail.com>
2023-02-17 17:16:05 -0800

    start KnightPathFinder

--- lib/knight_path_finder.rb
+++ lib/knight_path_finder.rb
@@ -5,10 +5,18 @@ class KnightPathFinder
   def initialize(start_pos)
     @start_pos = start_pos
     @root_node = PolyTreeNode.new(start_pos)
+    @size = 8*8
   end
 
   def build_move_tree()
 
   end
 
+  def self.valid_moves(pos)
+    row, col = pos
+
+    move = [row+2, col+1]
+    move_2 = [row+1,col+2]
+
+  end
 end
