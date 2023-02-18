def9ca0706cf4f51a521e9391f4e3d8f00a2f897
Ahmed <ahmed96razi39@gmail.com>
2023-02-17 16:59:55 -0800

    knights start

---
+++ lib/knight_path_finder.rb
@@ -0,0 +1,14 @@
+require_relative 'tree_node.rb'
+
+class KnightPathFinder
+
+  def initialize(start_pos)
+    @start_pos = start_pos
+    @root_node = PolyTreeNode.new(start_pos)
+  end
+
+  def build_move_tree()
+
+  end
+
+end
