f660fcfcfa2132f78141bbc9b8dfc52791dc728d
bird <mbird3@gmail.com>
2023-02-20 14:56:41 -0800

    generate valid positions

--- lib/knight_path_finder.rb
+++ lib/knight_path_finder.rb
@@ -8,11 +8,35 @@ class KnightPathFinder
     @start_pos = start_pos
     @root_node = PolyTreeNode.new(start_pos)
     @size = 8*8
+    @board = []
     @considered_positions = Set.new
   end
 
-  def build_move_tree()
+  def board_positions
+    h = 8
+    w = 8
+    (0...h).each do |i|
+      (0...w).each do |j|
+        @board << [i,j]
+      end
+    end
 
+    p @board
+  end
+
+  def build_move_tree()
+    # return self if @value == search_value
+    # q = [self]
+    # until q.empty?
+    #   check_node = q.shift
+    #   return check_node if check_node.value == search_value
+    #   q.concat(check_node.children)
+    # end
+    # nil
+
+    q = [root_node]
+    until q.empty?
+      q.new_move_positions({})
   end
 
   def self.valid_moves(pos)
@@ -32,6 +56,14 @@ class KnightPathFinder
     new_positions = valid_moves.reject { |el| @considered_positions.include?(el) } 
     new_positions
   end
+
+  
   
 end
 
+# pos = [0,0]
+# k = KnightPathFinder.new(pos)
+# p KnightPathFinder.valid_moves(pos)
+# k.considered_positions << [2,1]
+# p k.new_move_positions(pos)
+# k.board_positions
