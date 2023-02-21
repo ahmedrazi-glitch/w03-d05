84611f46569566463091a32e4095b6009010d4b7
bird <mbird3@gmail.com>
2023-02-20 16:40:49 -0800

    complete knight path

--- lib/knight_path_finder.rb
+++ lib/knight_path_finder.rb
@@ -27,6 +27,15 @@ class KnightPathFinder
   end
 
   def build_move_tree()
+    # return self if @value == search_value
+    # q = [self]
+    # until q.empty?
+    #   check_node = q.shift
+    #   return check_node if check_node.value == search_value
+    #   q.concat(check_node.children)
+    # end
+    # nil
+
     q = [@root_node]
     until q.empty?
       processing_node = q.shift
@@ -62,7 +71,29 @@ class KnightPathFinder
     new_board_positions
   end
 
-  
+  def find_path(pos)
+    res = []
+    processing_node = @root_node.bfs(pos)
+    res.unshift(processing_node.value)
+
+    until processing_node == @root_node
+      # if processing_node != @root_node
+      #   res.unshift(processing_node.value)
+      # else
+      #   res.unshift(@root_node.value)
+      # end
+      processing_node = processing_node.parent
+      res.unshift(processing_node.value)
+      # res.unshift(@root_node.value)
+    end
+
+    # until you reach the root
+    #   1. given a starting point
+    #   2. record parent
+    #   3. move to the parent
+    # res.unshift()
+    res
+  end
   
 end
 
@@ -73,4 +104,8 @@ k = KnightPathFinder.new(pos)
 # p k.new_move_positions(pos)
 # k.board_positions
 k.build_move_tree
-p k.considered_positions.length
+print k.root_node.bfs([6,4])
+p k.find_path([7, 6]) # => [[0, 0], [1, 2], [2, 4], [3, 6], [5, 5], [7, 6]]
+p k.find_path([6, 2]) # => [[0, 0], [1, 2], [2, 0], [4, 1], [6, 2]]
+#<PolyTreeNode:0x000000010c6a3dc8>%  
+#<PolyTreeNode:0x0000000104c64580>%
